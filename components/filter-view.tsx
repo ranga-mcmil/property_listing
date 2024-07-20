aaf2a99c3ba31a759d671e5818486d43ea4fb232
Ranga McMil <ranga@Rangas-MacBook-Air.local>
2024-07-20 15:12:09 +0200

    conditional stylying on filter buttons

--- components/filter-view.tsx
+++ components/filter-view.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 
+
 export function FilterViewButtons() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
@@ -20,13 +21,18 @@ export function FilterViewButtons() {
 
     replace(`${pathname}?${params.toString()}`);
 }
+  
+  let view = searchParams.get('view')
+  if (!view) {
+    view = "list"
+  }
 
   return (
     <div className="flex space-x-2">
-      <Button variant="outline" className="p-2" onClick={() => handleClick('list')}>
+      <Button variant={ view === 'list' ? "redish" : "outline" } className="p-2" onClick={() => handleClick('list')}>
         <List className="w-5 h-5" />
       </Button>
-      <Button variant="outline" className="p-2" onClick={() => handleClick('grid')}>
+      <Button variant={ view === 'grid' ? "redish" : "outline" } className="p-2" onClick={() => handleClick('grid')}>
         <LayoutGrid className="w-5 h-5" />
       </Button>
     </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { UserIcon } from "lucide-react";
 
 export function Navbar() {
 
@@ -10,27 +12,13 @@ export function Navbar() {
         </div>
         <nav className="flex items-center space-x-6 md:space-x-6">
           <div className="relative group hidden md:block">
-            <button className="text-gray-700">For Sale</button>
-            <div className="absolute left-0 hidden mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg group-hover:block">
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                Option 1
-              </a>
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                Option 2
-              </a>
-            </div>
+            <Link href="/listings?propertyType=for-sale">For Sale</Link>
+            
           </div>
           <div className="relative group hidden md:block">
-            <button className="text-gray-700">To Rent</button>
-            <div className="absolute left-0 hidden mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg group-hover:block">
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                Option 1
-              </a>
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                Option 2
-              </a>
-            </div>
+            <Link href="/listings?propertyType=to-rent">To Rent</Link>
           </div>
+
           <a href="#" className="text-gray-700 hidden md:block">
             New Developments
           </a>
@@ -64,22 +52,12 @@ export function Navbar() {
           </div>
         </nav>
         <div className="flex items-center space-x-4 hidden md:flex">
-          <button className="text-gray-700">Login</button>
           <div className="relative group">
-            <button className="flex items-center space-x-2 text-gray-700">
+            
+            <Button variant="ghost" className="flex items-center space-x-2">
               <span>Login</span>
-              <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-              </svg>
-            </button>
-            <div className="absolute right-0 hidden mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg group-hover:block">
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                Signup
-              </a>
-              <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                Login
-              </a>
-            </div>
+              <UserIcon className="w-5 h-5" />
+            </Button>
           </div>
         </div>
       </div>
